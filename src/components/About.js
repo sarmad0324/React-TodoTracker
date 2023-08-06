39ce8ef2463ecfcc5ce82d26ae48128fb7e56115
sarmad0324 <sarmadirfan78@gmail.com>
2023-08-07 00:34:49 +0500

    mm

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <div>
-      <p>Founder : Sarmad Irfan. CEO of Social Hub</p>
+      <p>Founder : Sarmad Irfan.</p>
       <a href='https://sarmad0324.github.io/portfolio/' target='blank' className='link'>Portfolio</a>
       <h4>Version 1.0.4</h4>
       <Link to='/' className='link'>Go Back</Link>
